c6f19923810e0a6e4d09132af2ef452c0dee460e
Joel Alves Pereira <atejap05@yahoo.com.br>
2023-12-19 14:58:08 -0300

    start food project

---
+++ src/app/meals/[slug]/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+type Props = {};
+
+const MealsDetails = (props: Props) => {
+  return (
+    <>
+      <div>MealsDetails</div>
+    </>
+  );
+};
+
+export default MealsDetails;
---
+++ src/app/meals/share/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ShareMealPage = (props: Props) => {
+  return <div>ShareMealPage</div>;
+};
+
+export default ShareMealPage;
